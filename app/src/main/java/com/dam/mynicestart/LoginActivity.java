baf2d6874a80ad5c05c89f1793f0935d4665aca3
carlokg <carlokg89@gmail.com>
2020-11-13 13:39:45 +0100

    login

--- app/src/main/java/com/dam/mynicestart/LoginActivity.java
+++ app/src/main/java/com/dam/mynicestart/LoginActivity.java
@@ -2,7 +2,9 @@ package com.dam.mynicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void register(View view) {
+        Intent i = new Intent(this, Registro.class);
+        startActivity(i);
     }
 }
