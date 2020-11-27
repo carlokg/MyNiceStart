4a6d1c22694731548986ec29508736e26c193369
carlokg <carlokg89@gmail.com>
2020-11-27 13:48:19 +0100

    --> animaciones/AppBar

--- app/src/main/java/com/dam/mynicestart/LoginActivity.java
+++ app/src/main/java/com/dam/mynicestart/LoginActivity.java
@@ -1,26 +1,43 @@
 package com.dam.mynicestart;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 
 public class LoginActivity extends AppCompatActivity {
+    ImageView mForest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+        mForest= findViewById(R.id.imageView);
+
+        Glide.with(this)
+                .load(R.drawable.chica)
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade(500))
+                .into(mForest);
+
+    }
     public void register(View view) {
         Intent i = new Intent(this, Registro.class);
         startActivity(i);
     }
-
     public void btnLogin(View view) {
         Intent i = new Intent(this, MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
         startActivity(i);
     }
 }
